90782f554186f20853014d89b1fe32906bbeca44
Scott Motte <mot@mot.la>
2023-05-22 08:36:22 -0700

    Demonstrate hello world

--- dotenv-vault-nextjs/pages/index.js
+++ dotenv-vault-nextjs/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello {process.env.NEXT_PUBLIC_HELLO}
         </h1>
 
         <p className={styles.description}>
