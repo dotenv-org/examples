ce09e36950b377eb9311996608a373acfcf02b97
Scott Motte <mot@mot.la>
2023-07-07 22:57:34 -0700

    Working fly.io example

--- nodejs/fly/index.js
+++ nodejs/fly/index.js
@@ -1,28 +1,12 @@
 // index.js
-// const PORT = process.env.PORT || 3000
-// const http = require('http')
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.end(`Hello ${process.env.HELLO}`)
-// })
-//
-// server.listen(PORT, () => {
-//   console.log(`Server running on port:${PORT}/`)
-// })
+const PORT = process.env.PORT || 3000
+const http = require('http')
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(`Hello ${process.env.HELLO}`)
+})
 
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node on Fly!</h1>";
-  name = req.params["name"];
-  if (name) {
-    res.send(greeting + "</br>and hello to " + name);
-  } else {
-    res.send(greeting);
-  }
-});
-
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
+server.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}/`)
+})
