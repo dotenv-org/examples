d8d64388d503f3406c026769016c39f8dfacb0de
Scott Motte <mot@mot.la>
2023-07-07 22:31:21 -0700

    Add express

--- nodejs/fly/index.js
+++ nodejs/fly/index.js
@@ -1,12 +1,28 @@
 // index.js
-const PORT = process.env.PORT || 3000
-const http = require('http')
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end(`Hello ${process.env.HELLO}`)
-})
+// const PORT = process.env.PORT || 3000
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.end(`Hello ${process.env.HELLO}`)
+// })
+//
+// server.listen(PORT, () => {
+//   console.log(`Server running on port:${PORT}/`)
+// })
 
-server.listen(PORT, () => {
-  console.log(`Server running on port:${PORT}/`)
-})
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get(["/", "/:name"], (req, res) => {
+  greeting = "<h1>Hello From Node on Fly!</h1>";
+  name = req.params["name"];
+  if (name) {
+    res.send(greeting + "</br>and hello to " + name);
+  } else {
+    res.send(greeting);
+  }
+});
+
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
