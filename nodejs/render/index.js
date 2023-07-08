0f7af889935e68fd64003a45e2b860247e9ae2c6
Scott Motte <mot@mot.la>
2023-07-07 23:23:29 -0700

    Add index.js

---
+++ nodejs/render/index.js
@@ -0,0 +1,12 @@
+// index.js
+const PORT = process.env.PORT || 3000
+const http = require('http')
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(`Hello ${process.env.HELLO}`)
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}/`)
+})
