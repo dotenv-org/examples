aa0b9f92c07fec846c880c4c268c0b4f42756161
Scott Motte <mot@mot.la>
2023-07-29 17:17:53 -0700

    Changes

--- remix/github-actions/app/routes/_index.tsx
+++ remix/github-actions/app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -7,10 +9,20 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  return json({
+    ENV: {
+      HELLO: process.env.HELLO, // HELLO="World" in .env file
+    },
+  });
+}
+
 export default function Index() {
+  const data = useLoaderData()
+
   return (
     <div>
-      Hello {process.env.NEXT_PUBLIC_HELLO}.
+      Hello {data.ENV.HELLO}.
     </div>
   );
 }
