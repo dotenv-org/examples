66907acaa6b5011777fd9f11bdba60659464b8ea
Scott Motte <mot@mot.la>
2023-07-30 12:40:37 -0700

    Serverless

---
+++ serverless/aws-lambda/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
+
+module.exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Hello ${process.env.HELLO}`,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
