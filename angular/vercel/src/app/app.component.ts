36a7e6a252a301c526195659cfbb045eb3667436
Scott Motte <mot@mot.la>
2023-08-02 16:35:16 -0700

    Add angular vercel guide

---
+++ angular/vercel/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'vercel';
+  NG_APP_HELLO = process.env['NG_APP_HELLO']
+}
