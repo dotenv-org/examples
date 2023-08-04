5359fa474738fa3f30c6e268e6c939bae93c19a7
Scott Motte <mot@mot.la>
2023-08-04 14:23:46 -0700

    Add dotenv

--- nodejs/cloud66/index.js
+++ nodejs/cloud66/index.js
@@ -1,4 +1,7 @@
 // index.js
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
+
 const PORT = process.env.PORT || 3000
 const http = require('http')
 const server = http.createServer((req, res) => {
