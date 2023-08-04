e7252640cc854e0052a7e69063d9a497ac881d2f
Scott Motte <mot@mot.la>
2023-08-04 11:59:27 -0700

    wip

---
+++ nodejs/aws-lambda/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
+
+exports.handler = function(event, context, callback) {
+  console.log(`Hello ${process.env.HELLO}.`)
+
+  callback(null, event)
+}
