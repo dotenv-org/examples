5d5ee9c4fe52b248beb5586c52f5572498ebbcc2
Scott Motte <mot@mot.la>
2023-10-19 18:04:10 -0700

    Add hello world steps

--- nuxtjs/vercel/app.vue
+++ nuxtjs/vercel/app.vue
@@ -1,5 +1,7 @@
+<script setup lang="ts">
+  const config = useRuntimeConfig()
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  Hello {{config.public.HELLO}}.
 </template>
--- nuxtjs/vercel/nuxt.config.ts
+++ nuxtjs/vercel/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      HELLO: process.env.HELLO
+    }
+  },
   devtools: { enabled: true }
 })
