fb18efb377ca9e507c6c2d0e54e2f64c2d23f534
Scott Motte <mot@mot.la>
2023-11-04 12:31:45 -0700

    Update example

--- php/heroku/index.php
+++ php/heroku/index.php
@@ -5,6 +5,4 @@
   $dotenv = DotenvVault\DotenvVault::createImmutable(__DIR__);
   $dotenv->safeLoad();
 
-  var_dump($_ENV); // remove this after you've confirmed it is working
-
   echo "Hello {$_ENV['HELLO']}";
