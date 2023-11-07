ff92a550eeaeaa990ac01c17e8112f2207840834
Scott Motte <mot@mot.la>
2023-11-07 07:57:49 -0800

    Add example code

---
+++ dotenv-blog/2023-11-07/REAMDE.md
@@ -0,0 +1,1 @@
+Example code from the blog at [Phpdotenv quirks and how to get around them](https://dotenv.org/blog/2023/11/07/)
---
+++ dotenv-blog/2023-11-07/immutable-safe-load.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+$env_hello = $_ENV['HELLO'];
+$server_hello = $_SERVER['HELLO'];
+$getenv_hello = getenv('HELLO');
+
+echo "ENV:    Hello {$env_hello}";
+echo "\n";
+echo "SERVER: Hello {$server_hello}";
+echo "\n";
+echo "getenv: Hello {$getenv_hello}";
