a7692e23b216f00cf5c03e5a6e825a83a29a50ee
Scott Motte <mot@mot.la>
2024-02-05 15:23:20 -0800

    inline comments with expand

---
+++ usage/dotenv-nodejs-inline-comments-expand/index.js
@@ -0,0 +1,15 @@
+var dotenv = require('dotenv')
+var dotenvExpand = require('dotenv-expand')
+
+var myEnv = dotenv.config()
+dotenvExpand.expand(myEnv)
+
+console.log(process.env.PASSWORD)
+console.log(process.env.DB_PASS)
+console.log(process.env.INLINE_COMMENTS)
+console.log(process.env.INLINE_COMMENTS_SINGLE_QUOTES)
+console.log(process.env.INLINE_COMMENTS_DOUBLE_QUOTES)
+console.log(process.env.INLINE_COMMENTS_BACKTICKS)
+console.log(process.env.INLINE_COMMENTS_SPACE)
+console.log(process.env.DB_PASSWORD)
+console.log(process.env.SECURE_TOKEN)
