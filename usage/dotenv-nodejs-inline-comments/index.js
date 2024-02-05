52b44cae7654b58194478c8780b93423d04686d5
Scott Motte <mot@mot.la>
2024-02-05 15:11:48 -0800

    inline comments example

---
+++ usage/dotenv-nodejs-inline-comments/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+console.log(process.env.INLINE_COMMENTS)
+console.log(process.env.INLINE_COMMENTS_SINGLE_QUOTES)
+console.log(process.env.INLINE_COMMENTS_DOUBLE_QUOTES)
+console.log(process.env.INLINE_COMMENTS_BACKTICKS)
+console.log(process.env.INLINE_COMMENTS_SPACE)
+console.log(process.env.DB_PASSWORD)
